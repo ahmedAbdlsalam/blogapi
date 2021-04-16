9ea06b8a6a09d64a80216ad2d887c5a49c90edf0
ahmedAbdlsalam <ahmedmoabdelsalam98@gmail.com>
2021-04-16 11:00:10 +0200

    second commit

--- posts/permissions.py
+++ posts/permissions.py
@@ -5,8 +5,8 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Read-only permissions are allowed for any request
         if request.method in permissions.SAFE_METHODS:
-            # print("Read-only permissions are allowed for any request")
+            print("Read-only permissions are allowed for any request")
             return True
         # Write permissions are only allowed to the author of a post
-        # print("Read-and-Write permissions are allowed for any request")
+        print("Read-and-Write permissions are allowed for any request")
         return obj.author == request.user
